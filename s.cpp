00c1e570721e27fb41bc71282ddca4654af5950a
yrxns <819924033@qq.com>
2023-05-30 19:17:48 +0800

    ...

--- s.cpp
+++ s.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 
-int main() { int s = 3; }
+int main() {
+    int s = 3;
+    int assssss = 4;
+    int ss = 5;
+    float d = 1;
+    std::cout << "..." << std::endl;
+
+    int aaaaaaaaaaaaa;
+    int aa;
+    double c;
+    vector<int> a;
+}
